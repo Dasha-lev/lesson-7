ba63c3bef22bb67c54ef702e1b8dfd63efeabb3b
Daria Levchuk <darina.levchuk@gmail.com>
2026-04-17 16:34:21 +0200

    change for backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-#terraform {
-  #backend "s3" {
-    #bucket         = "daria-levchuk-tf-state-2026" 
-    #key            = "lesson-5/terraform.tfstate"
-    #region         = "us-west-2"
-    #dynamodb_table = "terraform-locks"
-    #encrypt        = true
-  #}
-#}
+ terraform {
+   backend "s3" {
+     bucket         = "daria-levchuk-tf-state-2026" 
+     key            = "lesson-7/terraform.tfstate"
+     region         = "us-east-1"
+     dynamodb_table = "terraform-locks"
+     encrypt        = true
+   }
+ }
